2b04ef89a77e137c7ec7b433f0a72c3d82fed46d
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-26 22:23:04 +0545

    Resturants image added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/data/data.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
 import 'package:food_delivery_app/widgets/recent_order.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  _buildResturant() {
+    List<Widget> resturantList = [];
+    restaurants.forEach((Restaurant resturant) {
+      resturantList.add(Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(20.0),
+            border: Border.all(
+              width: 1.0,
+              color: Colors.grey[300]!,
+            )),
+        child: Image(
+            height: 150.0,
+            width: 150.0,
+            fit: BoxFit.fill,
+            image: AssetImage(resturant.imageUrl)),
+      ));
+    });
+    return Column(
+      children: resturantList,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +85,24 @@ class _HomeScreenState extends State<HomeScreen> {
                 )),
           ),
         ),
-        RecentOrder(),
+        const RecentOrder(),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text(
+                "Nearby Resturents ",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ),
+            _buildResturant()
+          ],
+        )
       ]),
     );
   }
